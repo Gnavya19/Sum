59bb1566c370389cf2d5af340b6748717947f2bb
Gnavya19 <navyaguddeti19@gmail.com>
2025-01-31 10:03:18 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    int a=10;
+    int b=20;
+    // public static void main(String[] args) {
+        // String a = "Hello world!";
+        // System.out.println(a);
+    }
+    
+
